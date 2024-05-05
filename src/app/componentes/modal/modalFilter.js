1ebe77b7eef450d33b83d1d14d9a00a92653e995
Jefferson <messias150401@gmail.com>
2024-05-05 14:01:47 -0300

    estilizando o modalFilter

--- src/app/componentes/modal/modalFilter.js
+++ src/app/componentes/modal/modalFilter.js
@@ -1,10 +1,16 @@
-import { View, Text, StyleSheet, TouchableOpacity, Pressable, Image} from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, Pressable, Image } from "react-native";
 import { useCallback, useEffect, useState } from 'react';
-import { InputRange } from "../InputRange";
 
+import RangeSlider, { Slider } from 'react-native-range-slider-expo';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import 'react-native-reanimated'
 
 export function ModalFilter({ handleClose }) {
     const [categoriasSelecionadas, setCategoriasSelecionadas] = useState([styles.textCategorias]);
+    const [fromValue, setFromValue] = useState(0);
+    const [toValue, setToValue] = useState(1000);
+    const [value, setValue] = useState(0);
+    const [duracaoSelecionada, setDuracaoSelecionada] = useState('');
 
     function toggleCategoria(categoria) {
         if (categoriasSelecionadas.includes(categoria)) {
@@ -15,15 +21,20 @@ export function ModalFilter({ handleClose }) {
             setCategoriasSelecionadas([...categoriasSelecionadas, categoria]);
         }
     }
-
+    function selecionarDuracao(duracao) {
+        setDuracaoSelecionada(duracaoSelecionada === duracao ? '' : duracao);
+    }
     // Função para verificar se uma categoria está selecionada
     function isCategoriaSelecionada(categoria) {
         return categoriasSelecionadas.includes(categoria);
     }
 
+
     return (
+
         <View style={styles.container}>
             <View style={styles.content}>
+                <Pressable onPress={handleClose} style={styles.imgs} ><Image style={styles.imgCard} source={require("../../../assets/close.png")} /></Pressable>
                 <Text style={styles.title}>Filtro de pesquisa</Text>
                 <View style={styles.areaCategoria}><Text style={styles.textCategoria}>Categorias</Text></View>
 
@@ -41,16 +52,187 @@ export function ModalFilter({ handleClose }) {
                     </Pressable>
                     {/* Adicione mais Pressables para outras categorias conforme necessário */}
                 </View>
+
+                <View style={styles.viewDuracao}>
+                    <Text style={styles.titlePreco}>Duração</Text>
+                    <View style={styles.alinhamento}>
+                        <Pressable onPress={() => selecionarDuracao('3-8 horas')}>
+                            <Text style={[styles.duracao, duracaoSelecionada === '3-8 horas' && styles.duracaoSelecionada]}>
+                                3-8 horas
+                            </Text>
+                        </Pressable>
+                        <Pressable onPress={() => selecionarDuracao('8-14 horas')}>
+                            <Text style={[styles.duracao, duracaoSelecionada === '8-14 horas' && styles.duracaoSelecionada]}>
+                                8-14 horas
+                            </Text>
+                        </Pressable>
+                        <Pressable onPress={() => selecionarDuracao('14-20 horas')}>
+                            <Text style={[styles.duracao, duracaoSelecionada === '14-20 horas' && styles.duracaoSelecionada]}>
+                                14-20 horas
+                            </Text>
+                        </Pressable>
+                        <Pressable onPress={() => selecionarDuracao('20-24 horas')}>
+                            <Text style={[styles.duracao, duracaoSelecionada === '20-24 horas' && styles.duracaoSelecionada]}>
+                                20-24 horas
+                            </Text>
+                        </Pressable>
+                    </View>
+                </View>
+                
                 <View>
                     <Text style={styles.titlePreco}>Preço</Text>
-                    <InputRange minValue ={0} maxValue={100} onChangeMin={(v)=>console.log} onChangeMax ={(v)=>console.log}/>
+
+
+
                 </View>
+                <GestureHandlerRootView style={styles.interact}>
+                    <View>
+
+                        <RangeSlider min={0} max={10000} step={10}
+                            fromValueOnChange={value => setFromValue(value)}
+                            toValueOnChange={value => setToValue(value)}
+                            initialFromValue={0}
+
+                            inRangeBarColor='#3D5CFF'
+                            outOfRangeBarColor='gray'
+                            fromKnobColor='#3D5CFF'
+                            toKnobColor="#3D5CFF"
+                        />
+                        <View style={styles.precos}><Text style={styles.textopreco}> R${fromValue} - </Text>
+                            <Text style={styles.textopreco}>R${toValue}</Text></View>
+
+                    </View>
+
+                    <View style={styles.distaceview}>
+                        <Text style={styles.titlePreco}>Distância</Text>
+                        <Slider min={0} max={50} step={1}
+                            valueOnChange={value => setValue(value)}
+                            initialValue={0}
+                            knobColor='#3D5CFF'
+
+                            inRangeBarColor='gray'
+                            outOfRangeBarColor='#3D5CFF'
+                        />
+                        <View style={styles.precos}>
+                            <Text style={styles.textopreco}>{value} Km</Text></View>
+                    </View>
+
+                    <View style={styles.buttonAreaFilter}>
+                    <TouchableOpacity style={[styles.buttonFilter, styles.buttonSaveClear]} onPress={handleClose}>
+                        <Text style={styles.buttonSaveTextClear}>Limpar</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={[styles.buttonFilter, styles.buttonSaveFilter]} onPress={handleClose}>
+                        <Text style={styles.buttonSaveTextFilter}>Aplicar Filtro</Text>
+                    </TouchableOpacity>
+                </View>
+
+                </GestureHandlerRootView>
+
+                
 
             </View>
         </View>
     );
 }
 const styles = StyleSheet.create({
+    buttonAreaFilter: {
+        flexDirection:"row",
+        width:"100%",
+        marginTop:60,
+        
+
+        justifyContent:"space-between"
+        
+    },
+    buttonFilter: {
+        flex:1,
+        alignItems:"center",
+        marginTop:14,
+        marginBottom:14,
+        padding:8,
+        height:50,
+        margin:10,
+        alignItems:'center',
+        justifyContent:'center',
+    },
+    buttonSaveFilter: {
+        backgroundColor: "#3D5CFF",
+        borderRadius: 8,
+    
+    },
+    buttonSaveClear: {
+        backgroundColor: "#FFF",
+        borderRadius: 8,
+        borderWidth:1,
+    
+    },
+    buttonSaveTextFilter: {
+        color: "#FFFFFF",
+        fontFamily: "Poppins-Bold",
+        fontSize: 20,
+        
+    },
+    buttonSaveTextClear: {
+        color: "#000",
+        fontFamily: "Poppins-Bold",
+        fontSize: 20,
+        
+    },
+    imgCard: {
+        marginLeft: 5,
+        width: 20,
+        height: 20,
+    },
+
+    alinhamento: {
+        flexDirection: "row",
+        marginLeft: 20
+    },
+    duracao: {
+
+        fontFamily: 'Poppins-Regular',
+        color: '#1F1F39',
+        marginBottom: 10,
+        marginRight: 5,
+        padding: 4,
+        backgroundColor: "gray",
+        borderRadius: 10,
+
+        borderColor: '#1F1F39',
+    },
+    duracaoSelecionada: {
+
+        fontFamily: 'Poppins-Regular',
+        color: '#1F1F39',
+        marginBottom: 10,
+        backgroundColor: "#3D5CFF",
+        marginRight: 5,
+        padding: 4,
+        borderRadius: 10,
+
+        borderColor: '#1F1F39',
+    },
+    viewduração: {
+
+        marginLeft: 10,
+        marginTop: 500,
+        position: "absolute"
+    },
+    distaceview: {
+        marginTop: 80
+    },
+    textopreco: {
+        fontFamily: "Poppins-Regular",
+    },
+    precos: {
+        flexDirection: "row",
+        paddingLeft: "40%"
+    },
+    interact: {
+        height: "100%",
+        width: "100%"
+    },
     titlePreco: {
         fontFamily: "Poppins-Bold",
         color: "#1F1F39",
@@ -65,8 +247,9 @@ const styles = StyleSheet.create({
         margin: 5
     },
     textCategorias: {
+        fontSize: 16,
         marginRight: 5,
-        backgroundColor: "#FFF",
+        backgroundColor: "gray",
         borderRadius: 10,
         padding: 5,
         marginLeft: 20
@@ -74,8 +257,9 @@ const styles = StyleSheet.create({
 
     },
     textCategoriasSelecionada: {
+        fontSize: 16,
         marginRight: 5,
-        backgroundColor: "#F00",
+        backgroundColor: "#3D5CFF",
         borderRadius: 10,
         padding: 5,
         marginLeft: 20
